fc61466e8c5d7ac2126665744bf38940ac6d6267
Samia Hebaz <shebaz@c3r6p8.1337.ma>
2024-05-29 14:29:35 +0100

    125

--- check_arguments.c
+++ check_arguments.c
@@ -1,87 +1,94 @@
 #include "so_long.h"
 
-void affiche_map(t_data *data)
+void clear_exit(t_data *data)
 {
-	// printf("check\n");
-	printf("map_height = %d\n", data->map_height);
-	printf("map_width = %d\n", data->map_width);
-	int i = 0;
-	int j;
-	while (i < data->map_height)
+	clear_data(data, 0);
+	exit(1);
+}
+void check_form(t_data *data, char **av)
+{
+	int		fd;
+	char	*str;
+	int		i;
+	int		width;
+
+	width = 0;
+	fd = open(av[1], O_RDWR, 0777);
+	if (fd == -1)
+		clear_exit(data);
+	width = data->map_width;
+	i = 0;
+	while (1)
 	{
-		j = 0;
-		while(j < data->map_width)
-		{
-			printf("%c",data->map[i][j]);
-			j++;
-		}
+		str = get_next_line(fd);
+		if(!str || width != ft_strlen(str))
+			break;
+		free(str);
 		i++;
 	}
+	free(str);
+	if ((i != (data->map_height - 1) || i == (data->map_height - 1) && strlen(data->map[data->map_height - 1]) != width - 1) && data->map_height != 1)
+	{
+		perror("Error : map is not a rectangle");
+		clear_exit(data);
+	}
 }
-// int calcul_line_length(char *str)
+void	check_parameters_number(t_data *data, int p_nbr, int e_nbr)
+{
+	if (p_nbr > 1 || e_nbr > 1)
+	{
+		perror("Error : more than expected");
+		clear_data(data, 0);
+		exit(1);
+	}
+	if (data->coins_nbr < 1)
+	{
+		perror("Error : At Least one coin required");
+		clear_data(data, 0);
+		exit(1);
+	}
+}
+
+void check_borders(t_data *data)
+{
+	int i;
+	int j;
+	int tmp;
+
+	i = 0;
+	j = 0;
+	tmp = 1;
+	while (j < data->map_width - 1)
+	{
+		if (data -> map[0][j] != '1' || data ->map[data->map_height - 1][j] != '1')
+			tmp = 0;
+		j++;
+	}
+	while (i < data->map_height)
+	{
+		if(data-> map[i][0] != '1' || data ->map[i][data->map_width - 2] != '1')
+			tmp = 0;
+		i++;
+	}
+	if (!tmp)
+	{
+		perror("Error");
+		clear_exit(data);
+	}
+}
+
 void check_arguments(t_data *data, char **av)
 {
     int	i;
     int	j;
-    int fd;
-	int	width;
-	char	*str;
-	int		p_nbr;
-	int		e_nbr;
+	int	p_nbr;
+	int	e_nbr;
 
-	int height = data->map_height;
+	i = 0;
 	p_nbr = 0;
 	e_nbr = 0;
-	// printf("map_height = %d\n",data->map_height);
-	width = ft_strlen(data->map[0]);
-    i = 1;
-	printf("main_width = %d\n",width);
-	affiche_map(data);
-	while (data->map[i] && i < height)
-	{
-		printf("i = %d\n", i);
-		printf("main_width = %d\n",ft_strlen(data->map[i]));
-		if (width != ft_strlen(data->map[i]))
-			break;
-		i++;
-	}
-	printf("i = %d\n", i);
-	//printf("limit = %d\n", data->map_height - 1);
-	if (i == (data->map_height) - 1)
-		return ;
-	else
-	{
-		perror("Error : map is not a rectangle");
-		clear_data(data);
-		exit(1);
-	}
-	//printf("hey");
-	///calcul if the map's form is rectangle
-
-	// fd = open(av[1], O_RDWR, 0777);
-	// if(fd == -1)
-	// {
-	// 	clear_data(data);
-	// 	exit(1);
-	// }
-	// str = get_next_line(fd);
-	// width = ft_strlen(str);
-	// printf("width_to_compare = %d\n", width);
-	// while (1)
-	// {
-	// 	str = get_next_line(fd);
-	// 	printf("width = %d\n", ft_strlen(str));
-	// 	if(!str)
-	// 		break;
-	// 	if (width != ft_strlen(str))
-	// 	{
-	// 		perror("Error : map is not a rectangle");
-	// 		clear_data(data);
-	// 		exit(1);
-	// 	}
-	// }
-
-	///
+	//check_form(data, av);
+	check_borders(data);
     while (i < data->map_height)
     {
         j = 0;
@@ -91,18 +98,27 @@ void check_arguments(t_data *data, char **av)
 				p_nbr++;
 			if (data->map[i][j] == 'E')
 				e_nbr++;
+			j++;
         }
+		i++;
     }
-	if (p_nbr > 1 || e_nbr > 1)
-	{
-		perror("Error : more than expected");
-		clear_data(data);
-		exit(1);
-	}
-	if (data->coins_nbr < 1)
-	{
-		perror("Error : At Least one coin required");
-		clear_data(data);
-		exit(1);
-	}
+	check_parameters_number(data, p_nbr,e_nbr);
 }
+// void affiche_map(t_data *data)
+// {
+// 	// printf("check\n");
+// 	printf("map_height = %d\n", data->map_height);
+// 	printf("map_width = %d\n", data->map_width);
+// 	int i = 0;
+// 	int j;
+// 	while (i < data->map_height)
+// 	{
+// 		j = 0;
+// 		while(j < data->map_width)
+// 		{
+// 			printf("%c",data->map[i][j]);
+// 			j++;
+// 		}
+// 		i++;
+// 	}
+// }
--- main.c
+++ main.c
@@ -6,42 +6,46 @@
 /*   By: shebaz <shebaz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 10:34:15 by shebaz            #+#    #+#             */
-/*   Updated: 2024/05/28 13:12:48 by shebaz           ###   ########.fr       */
+/*   Updated: 2024/05/29 14:28:46 by shebaz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void free_2d_array(char **array, int rows) {
-    for (int i = 0; i < rows; ++i) {
+void free_2d_array(char **array, int rows) //work
+{
+    int i;
+    
+    i = 0;
+    while (i < rows)
+    {
         free(array[i]);
+        i++;
     }
     free(array);
 }
-void clear_data(t_data *data)
+
+void clear_data(t_data *data, int i)//work
 {
     if (data->map)
         free_2d_array(data->map, data->map_height);
-    if (data->mlx_ptr) {
-        if (data->win_ptr) {
+    if (data->mlx_ptr)
+    {
+        if (data->win_ptr)
             mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-        }
-        if (data->wall) {
+        if (data->wall)
             mlx_destroy_image(data->mlx_ptr, data->wall);
-        }
-        if (data->road) {
+        if (data->road)
             mlx_destroy_image(data->mlx_ptr, data->road);
-        }
-        if (data->coins) {
+        if (data->coins)
             mlx_destroy_image(data->mlx_ptr, data->coins);
-        }
-        for (int i = 0; i < 3; ++i) {
-            if (data->player[i]) {
-                mlx_destroy_image(data->mlx_ptr, data->player[i]);
-            }
-        }
-        if (data->exit) {
+        if (data->exit)
             mlx_destroy_image(data->mlx_ptr, data->exit);
+        while (i < 3)
+        {
+            if (data->player[i])
+                mlx_destroy_image(data->mlx_ptr, data->player[i]);
+            i++;
         }
         mlx_destroy_display(data->mlx_ptr);
         free(data->mlx_ptr);
@@ -89,7 +93,9 @@ void map_2d_array(t_data *data, char **av, int width, int height) {
 		y = 0;
         while (y < width && y < WINDOWS_WIDTH) {
             data->map[x][y] = str[y];
-			y++;
+            if(str[y] == 'C' || str[y] == 'c')
+                data->coins_nbr++;
+    		y++;
         }
 		data->map[x][width] = '\0';
         free(str);
@@ -98,7 +104,8 @@ void map_2d_array(t_data *data, char **av, int width, int height) {
     close(fd);
 }
 
-void draw_map(t_data *data, int width, int height) {
+void draw_map(t_data *data, int width, int height)
+{
     int y;
 	int	x;
 
@@ -154,7 +161,7 @@ void go_up(t_data *data)
     }
     else if (data->map[data->x - 1][data->y] == 'E' && !(data->coins_nbr))
 	{
-		clear_data(data);
+		clear_data(data, 0);
         exit(0);
 	}
 }
@@ -182,7 +189,7 @@ void go_down(t_data *data)
     }
     else if (data->map[data->x][data->y] == 'E' && data->coins_nbr == 0)
 	{
-		clear_data(data);
+		clear_data(data, 0);
         exit(0);
 	}
 }
@@ -210,7 +217,7 @@ void go_right(t_data *data)
     }
     else if (data->map[data->x][data->y + 1] == 'E' && data->coins_nbr == 0)
 	{
-		clear_data(data);
+		clear_data(data, 0);
         exit(0);
 	}
 }
@@ -238,26 +245,44 @@ void go_left(t_data *data)
     }
     else if (data->map[data->x][data->y - 1] == 'E' && data->coins_nbr == 0)
 	{
-		clear_data(data);
+		clear_data(data, 0);
         exit(0);
 	}
 }
-int handle_key(int keycode, t_data *data)
+int handle_key(int keycode, t_data *data)//work
 {
     if (keycode == 65307)//ESC
     {
-		clear_data(data);
+		clear_data(data, 0);
 		exit(0);
 	}
-    else if(keycode == 0x0077)//W
+    else if (keycode == 0x0077)//W
         go_up(data);
-    else if(keycode == 0x0073)//S
+    else if (keycode == 0x0073)//S
         go_down(data);
-    else if(keycode == 0x0061)//A
+    else if (keycode == 0x0061)//A
         go_left(data);
-    else if(keycode == 0x0064)//D
+    else if (keycode == 0x0064)//D
         go_right(data);
-    return 0;
+    return (0);
+}
+void initial_struct(t_data *data)//work
+{
+    int i;
+
+    i = 0;
+    data->coins = NULL;
+    data->wall = NULL;
+    while(i < 3)
+    {
+       data->player[i] = NULL;
+       i++;
+    }
+    data->exit = NULL;
+    data->road = NULL;
+    data->win_ptr = NULL;
+    data->mlx_ptr = NULL;
+    data->map = NULL;
 }
 
 int main(int ac, char **av)
@@ -273,7 +298,7 @@ int main(int ac, char **av)
     data = malloc(sizeof(t_data));
     if (!data)
         return 1;
-
+    initial_struct(data);
     int fd = open(av[1], O_RDONLY);
     if (fd == -1)
     {
@@ -286,11 +311,12 @@ int main(int ac, char **av)
     {
         perror("Error reading file");
         close(fd);
-        clear_data(data);
+        clear_data(data, 0);
         return 1;
     }
     width = strlen(str);
     data->map_width = width;
+    data->coins_nbr = 0;
     height = 1;
     free(str);
 
@@ -299,16 +325,16 @@ int main(int ac, char **av)
         height++;
         free(str);
     }
-    data->map_height = height;
     close(fd);
+    data->map_height = height;
+    map_2d_array(data, av, width, height);
+    check_arguments(data,av);
     data->mlx_ptr = mlx_init();
     if (!data->mlx_ptr)
     {
         free(data);
         return 1;
     }
-    map_2d_array(data, av, width, height);
-    check_arguments(data,av);
 	data->win_ptr = mlx_new_window(data->mlx_ptr, width * TILE_SIZE, height * TILE_SIZE, "So_long");
     if (!data->win_ptr)
     {
@@ -330,13 +356,12 @@ int main(int ac, char **av)
     if (!data->wall || !data->road || !data->coins || !data->player[0] || !data->exit)
     {
         perror("Failed to load one or more textures");
-        clear_data(data);
+        clear_data(data, 0);
         return 1;
     }
-
     draw_map(data, width, height);
     mlx_key_hook(data->win_ptr, handle_key, data);
     mlx_loop(data->mlx_ptr);
-    clear_data(data);
+    clear_data(data, 0);
     return 0;
 }
